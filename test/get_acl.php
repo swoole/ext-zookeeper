68ae18100da7e80c79813fc40ade9bd5e896183a
hihozhou <hihozhou@gmail.com>
2019-04-24 12:08:22 +0800

    get_acl

---
+++ test/get_acl.php
@@ -0,0 +1,9 @@
+<?php
+use swoole\zookeeper;
+
+go(function () {
+    zookeeper::setDebugLevel(1);
+    $zk = new zookeeper("127.0.0.1:2181", 2.5);
+    var_dump("create", $zk->create("/test_acl", 'acl test'), $zk->errCode);
+    var_dump("get_acl", $zk->get_acl("/test_acl"), $zk->errCode);
+});
