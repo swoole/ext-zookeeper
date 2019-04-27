6c18a64cc82f49751c7583fa8f7de77a3790ce7a
zhanglei <zhanglei1@chelun.com>
2019-04-27 15:26:56 +0800

    Merge remote-tracking branch 'upgrade/master' into zhanglei1
    # Conflicts:
    #	.gitignore
    #	src/main.cpp

--- test/test_acl.php
+++ test/test_acl.php
@@ -4,23 +4,29 @@ use swoole\zookeeper;
 go(function () {
     zookeeper::setDebugLevel(1);
     $zk = new zookeeper("127.0.0.1:2181", 2.5);
-    $a = $zk->setAcl("/test34",[
+
+
+    $path = "/test234";
+    $zk->create($path, 'rango');
+
+    $re = $zk->addAuth("digest","zookeeper:zookeeper");
+    var_dump($re);
+
+    $r = $zk->get($path);
+    var_dump($r);
+
+
+    $a = $zk->setAcl($path,[
         [
             "perms"=>zookeeper::PERM_READ,
             "id"=>[
-                "id"=>"anyone",
-                "scheme"=>"world"
-            ]
-        ],
-        [
-            "perms"=>zookeeper::PERM_READ,
-            "id"=>[
-                "id"=>"anyone",
-                "scheme"=>"world"
+                "id"=>"zookeeper:zookeeper",
+                "scheme"=>"auth"
             ]
         ]
     ]);
     var_dump($a);
-    var_dump($zk->errCode);
-    var_dump($zk->getAcl("/test34"));
+            var_dump($zk->errCode);
+
+    //var_dump($zk->getAcl("/test34"));
 });
