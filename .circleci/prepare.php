64303d9208153b86bed8528d8ec9fa744a9fecac
minbaby.zhang <zhangshaomin_1990@126.com>
2019-04-28 18:03:26 +0800

    换个方式搞一波

--- .circleci/prepare.php
+++ .circleci/prepare.php
@@ -9,7 +9,7 @@ function getResource($name, $url)
     echo "{$root}/data/{$name}", '_____';
     if (!file_exists("{$root}/data/{$name}")) {
         echo 'fetch:', $name, PHP_EOL;
-        file_put_contents("{$root}/data/{$name}", file_get_contents($url));
+        echo `wget -O {$root}/data/{$name} {$url}`;
     } else {
         echo 'ign:', $name, PHP_EOL;
     }
@@ -41,5 +41,5 @@ foreach ($data as $value) {
         getResource($name, $url);
     }
     getResource("{$value['name']}.zip", "https://github.com/swoole/{$value['name']}/archive/v{$value['version']}.zip");
-    echo `unzip {$root}/data/{$value['name']}.zip -d {$root}/data/{$value['name']} && cd {$root}/data/{$value['name']}/{$value['name']}-{$value['version']} && {$value['cmd']}`;
+    echo `unzip -q {$root}/data/{$value['name']}.zip -d {$root}/data/{$value['name']} && cd {$root}/data/{$value['name']}/{$value['name']}-{$value['version']} && {$value['cmd']}`;
 }
